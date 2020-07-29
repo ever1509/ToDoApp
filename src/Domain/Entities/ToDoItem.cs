5fafd9c4d21a657148682ad52c68d95238798543
orelle01 <eorellana@applaudostudios.com>
2020-07-28 21:11:55 -0600

    updated

--- src/Domain/Entities/ToDoItem.cs
+++ src/Domain/Entities/ToDoItem.cs
@@ -3,7 +3,7 @@ using Domain.Enums;
 
 namespace Domain.Entities
 {
-    public class ToDoItem_
+    public class ToDoItem
     {
         public int Id { get; set; }
         public string Title { get; set; }
