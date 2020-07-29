558b226aec265c9a78f87c4b9d0fdfd33d727d6e
orelle01 <eorellana@applaudostudios.com>
2020-07-28 21:10:02 -0600

    Added changes for Domain project

---
+++ src/Domain/Entities/ToDoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Domain.Enums;
+
+namespace Domain.Entities
+{
+    public class ToDoItem_
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Note { get; set; }
+        public bool Done { get; set; }
+        public DateTime? Reminder { get; set; }
+        public PriorityLevel PriorityLevel { get; set; }
+    }
+}
